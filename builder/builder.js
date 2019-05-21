8caeaa5173f03f7ad11e6bdf8fb44b5ed1d7dd3b
Pedro Heliodoro <webmaster@ideiasfrescas.com>
2019-05-21 13:19:31 +0100

    more buttons

--- builder/builder.js
+++ builder/builder.js
@@ -2,15 +2,24 @@ var css = require('css');
 var fs = require('fs');
 
 var k = fs.readFileSync("../_alpha/geral.css");
-
-
+    
+    //ler todo o css a partir das folhas de estilo
 var data = fs.readFileSync("../portateis/geral.css");
+data += fs.readFileSync("../tablets/galaxy.css");
+data += fs.readFileSync("../tablets/ipad.css");
+data += fs.readFileSync("../tablets/kindle.css");
+data += fs.readFileSync("../tablets/nexus.css");
+data += fs.readFileSync("../smartphone/galaxy.css");
+data += fs.readFileSync("../smartphone/htc.css");
+data += fs.readFileSync("../smartphone/iphones.css");
+data += fs.readFileSync("../smartphone/pixel.css");
+data += fs.readFileSync("../smartphone/windows.css");
 
 var ast = css.parse(data.toString("utf-8"));
-var result = css.stringify(ast, { sourcemap: true });
+//var result = css.stringify(ast, { sourcemap: true });
 
 var ast2 = css.parse(k.toString("utf-8"));
-var result2 = css.stringify(ast2, { sourcemap: true });
+//var result2 = css.stringify(ast2, { sourcemap: true });
 
 var css = '';
 ast.stylesheet.rules.forEach(regra => {
